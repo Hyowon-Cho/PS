878782937411f465d302636920b8369b71b59189
Lukas Cho <63900561+Hyowon-Cho@users.noreply.github.com>
2022-08-05 14:35:50 +0900

    1260
    Review 1260 (DFS / BFS)

---
+++ Backjoon/1260.py
@@ -0,0 +1,40 @@
+import sys
+from collections import deque
+input = lambda : sys.stdin.readline()
+
+n,m,start=map(int,input().split()) # We input the data.
+visited=[False]*(n+1) # We set visited to False.
+
+graph=[[] for _ in range(n+1)] # Mapping the graph.
+
+for _ in range(m):
+    a,b=map(int,input().split())
+    graph[a].append(b)
+    graph[b].append(a)
+
+# If it has more than one vertex that we can visit, we need to visit the smaller vertex one first.
+for i in range(len(graph)):
+    graph[i].sort() # We sorted.
+
+def dfs(start):
+    print(start, end=' ')
+    visited[start]=True # The starting point should be True
+    for i in graph[start]: # Since starting point, we start to search insdie the graph.
+        if not visited[i]: # For every section that we didn't visit, we use recursion(dfs) .
+            dfs(i)
+            visited[i]=True # Now we set as True.
+
+def bfs(start):
+    q=deque([start])
+    visited[start]=True
+    while q:
+        now=q.popleft()
+        print(now,end=' ')
+        for i in graph[now]:
+            if not visited[i]:
+                q.append(i)
+                visited[i]=True
+dfs(start)
+visited=[False]*(n+1)
+print()
+bfs(start)
